b861a80ae10c2632c887140faee47736954ae906
Plazo-09 <abayew.arslan@gmail.com>
2023-12-11 10:33:55 +0500

    feat: change tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,30 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // это пример цветов во время верстки добавляй цвета сразу в переменные
+      // а если название состоит из двух слов давай юзать camelCase чтобы не путать с цветами по умолчанию
+      colors: {
+        'blue': '#1fb6ff',
+        'gray-light': '#d3dce6', 
       },
+      // тут происходит переопределение брейкпоинтов по умолчанию на кастомные
+      // используй также как и обычные просто сначала делаешь desktop а когда надо например меньше чем 767px, "md:p-[10px]"
+      screens: {
+        '2xl': {'max': '1535px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      }
     },
   },
   plugins: [],
