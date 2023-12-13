425f1099756753d1ba733ffb26ab1235d55215d0
RahymCharyyev <r569837@gmail.com>
2023-12-13 21:19:14 +0500

    ✨feature: Test slider

--- src/widgets/home/main-slider/MainSlider.tsx
+++ src/widgets/home/main-slider/MainSlider.tsx
@@ -1,3 +1,32 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import leaf from '@/images/leaf.png';
+import clouds from '@/images/clouds.jpg';
+
+import 'swiper/css';
+import Image from 'next/image';
+
 export const MainSlider = () => {
-  return <div>MainSlider</div>;
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>
+        <Image src={clouds} alt="" width={50} height={100} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={leaf} alt="" width={50} height={100} />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={clouds} alt="" width={50} height={100} />{' '}
+      </SwiperSlide>
+      <SwiperSlide>
+        <Image src={leaf} alt="" width={50} height={100} />
+      </SwiperSlide>
+    </Swiper>
+  );
 };
