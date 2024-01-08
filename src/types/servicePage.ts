87e9d4c0db3239f153f6fe42034b7951018af8ae
RahymCharyyev <r569837@gmail.com>
2024-01-08 21:59:43 +0500

    linteer autoprefix

--- src/types/servicePage.ts
+++ src/types/servicePage.ts
@@ -32,82 +32,82 @@ export interface Event {
   image2Path: string;
 }
 
-// export interface MaintenanceEvents {
-//   firstImagePath: string;
-//   secondImagePath: string;
-//   title?: string;
-//   subtitle?: string;
-//   text?: string;
-// }
+export interface MaintenanceEvents {
+  firstImagePath: string;
+  secondImagePath: string;
+  title?: string;
+  subtitle?: string;
+  text?: string;
+}
 
-// export interface MaintenanceRegister {
-//   title?: string;
-//   name: string;
-//   phone: string;
-//   mail: string;
-//   message?: string;
-//   phoneService: string;
-//   phoneSale: string;
-// }
+export interface MaintenanceRegister {
+  title?: string;
+  name: string;
+  phone: string;
+  mail: string;
+  message?: string;
+  phoneService: string;
+  phoneSale: string;
+}
 
-// export interface MaintenanceWarranty {
-//   id: number;
-//   title: string;
-//   description: string;
-// }
+export interface MaintenanceWarranty {
+  id: number;
+  title: string;
+  description: string;
+}
 
-// export interface MaintenanceService {
-//   id: number;
-//   title: string;
-//   description: string;
-//   subtitle: string;
-//   accordion: {
-//     id: number;
-//     name: string;
-//     text: string;
-//   }[];
-//   userGuideTitle: string;
-//   tableHead: {
-//     year: string;
-//     type: string;
-//     state: string;
-//     name: string;
-//     createdAt: string;
-//     downloadLink: string;
-//   };
-//   tableContent: {
-//     id: number;
-//     year: string;
-//     type: string;
-//     state: string;
-//     name: string;
-//     createdAt: string;
-//     downloadLink: string;
-//   }[];
-// }
+export interface MaintenanceService {
+  id: number;
+  title: string;
+  description: string;
+  subtitle: string;
+  accordion: {
+    id: number;
+    name: string;
+    text: string;
+  }[];
+  userGuideTitle: string;
+  tableHead: {
+    year: string;
+    type: string;
+    state: string;
+    name: string;
+    createdAt: string;
+    downloadLink: string;
+  };
+  tableContent: {
+    id: number;
+    year: string;
+    type: string;
+    state: string;
+    name: string;
+    createdAt: string;
+    downloadLink: string;
+  }[];
+}
 
-// export interface MaintenanceMap {
-//   title: string;
-//   tableHead: {
-//     models: string;
-//     map: string;
-//   };
-//   models: {
-//     id: number;
-//     name: string;
-//   }[];
-//   downloadLink: string;
-// }
+export interface MaintenanceMap {
+  title: string;
+  tableHead: {
+    models: string;
+    map: string;
+  };
+  models: {
+    id: number;
+    name: string;
+  }[];
+  downloadLink: string;
+}
 
-// export interface MaintenanceResponsibility {
-//   title: string;
-//   subtitle: string;
-//   text: string;
-//   listItems: {
-//     id: number;
-//     listText: string;
-//   }[];
-// }
+export interface MaintenanceResponsibility {
+  title: string;
+  subtitle: string;
+  text: string;
+  listItems: {
+    id: number;
+    listText: string;
+  }[];
+}
 
 export interface Stock {
   id: number;
